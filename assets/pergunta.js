acc5a5c91703314972c5a55088125d3c84b0ba03
robinho-c <159184830+robinho-c@users.noreply.github.com>
2024-10-01 16:40:33 -0300

    01/10

---
+++ assets/pergunta.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'programação',
+    'o que python',
+    'python é uma linguagem de programação',
+)
+
+
+criaCartão(
+    'geografia',
+    'qual é a capital da França',
+    'a capital da França é Paris'
+)
+
+
+
+criaCartao(
+    'ptrogramação',
+    'o que é uma função',
+    'uma função é um bloco de código que executa alguma tarefa'
+)
